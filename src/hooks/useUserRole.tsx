556c9cb48b9072fb864d9a78d564823122da4035
Chirag Sharma <chiragnoida3345@gmail.com>
2025-05-28 18:50:20 +0530

    Completed Landing Page and Hero Section

--- src/hooks/useUserRole.tsx
+++ src/hooks/useUserRole.tsx
@@ -13,7 +13,7 @@ export const useUserRole = () => {
 
   return {
     isLoading,
-    isInterviewer: userData?.role === "interviewer",
-    isCandidate: userData?.role === "candidate",
+    isInterviewer: userData?.role === "Interviewer",
+    isCandidate: userData?.role === "Candidate",
   };
 };
