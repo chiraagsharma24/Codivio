5e65c87266dd09b8c4644fce974a79da7bf16323
Chirag Sharma <chiragnoida3345@gmail.com>
2025-05-29 16:26:56 +0530

    Creating MeetingRoom route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
